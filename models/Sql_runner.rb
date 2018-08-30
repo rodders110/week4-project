a16b3022b1f3d5127c771fd5ba9fae3ab0db3302
Roddy Daly <roddy_daly110@hotmail.com>
2018-08-30 12:11:01 +0100

    final commit

--- models/Sql_runner.rb
+++ models/Sql_runner.rb
@@ -3,7 +3,7 @@ require('pg')
 class SqlRunner
   def self.run(sql, values = [])
     begin
-      db = PG.connect ({dbname: 'sweet_shoppe', host: 'localhost'})
+      db = PG.connect ({dbname: 'da74bq6l589363', host: 'ec2-54-83-51-78.compute-1.amazonaws.com', port: 5432, user: 'cscdkosdgnulnh', password: '03aa4a3ae41d420d16c286105da26388bfed41d53763b1b53fe83210f650e8fb'})
       db.prepare("query", sql)
       result = db.exec_prepared("query", values)
     ensure
